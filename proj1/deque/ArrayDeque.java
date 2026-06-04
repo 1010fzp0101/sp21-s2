fe40d08862f418b485213786e750ab2eebfcf1bf
John <450339151ppzzff@gmail.com>
2026-06-04 10:15:13 +0800

    Deque finished

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,7 +1,171 @@
 package deque;
 
 public class ArrayDeque<T> {
-    private class Deque {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] arr;
+    private int front;
+
+    public ArrayDeque() {
+        size = 0;
+        arr = (T[])new Object[8];
+        nextFirst = 4;
+        nextLast = 5;
+        front = nextLast;
+    }
+
+    private void resize (int capacity) {
+        if (capacity > arr.length) {
+            T[] a = (T[]) new Object[capacity];
+            System.arraycopy(arr, 0, a, 0, nextLast);
+            System.arraycopy(arr, nextLast, a, capacity - size + nextLast, size - nextLast);
+            arr = a;
+            nextFirst = arr.length - (size - nextFirst);
+            front = IncreaseIndex(nextFirst);
+        }
+        else if (capacity < arr.length) {
+            if (nextFirst >= 1 + nextLast) {
+                T[] a = (T[]) new Object[capacity];
+                System.arraycopy(arr, 0, a, 0, nextLast + 1);
+                System.arraycopy(arr, nextFirst + 1, a, a.length - (arr.length - nextFirst) + 1, arr.length - nextFirst - 1);
+                int m = arr.length;
+                arr = a;
+                nextFirst = arr.length - (m - nextFirst);
+                front = IncreaseIndex(nextFirst);
+            } else {
+                T[] a = (T[]) new Object[capacity];
+                System.arraycopy(arr, nextFirst + 1, a, 0, nextLast - nextFirst - 1);
+                arr = a;
+                nextLast = nextLast - nextFirst - 1;
+                nextFirst = capacity - 1;
+                front = IncreaseIndex(nextFirst);
+            }
+        }
+    }
+
+    public void addFirst(T item) {
+        if (size == arr.length) {
+            resize(size * 2);
+        }
+        arr[nextFirst] = item;
+        front = nextFirst;
+        nextFirst = DecreaseIndex(nextFirst);
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == arr.length) {
+            resize(size * 2);
+        }
+        if (arr[nextFirst] != null) {
+            front = nextFirst;
+        }
+        arr[nextLast] = item;
+        nextLast = IncreaseIndex(nextLast);
+        size += 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public T get(int index) {
+        return arr[(front + index) % arr.length];
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; ++i) {
+            System.out.print(get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (arr.length >= 16) {
+            if ((float)(size - 1) / arr.length < 0.25) {
+                resize(arr.length / 2);
+            }
+            front = IncreaseIndex(nextFirst);
+            T elmt = arr[front];
+            arr[front] = null;
+            nextFirst = front;
+            front = IncreaseIndex(nextFirst);
+            size -= 1;
+            return elmt;
+        } else {
+            if (size == 0) {
+                return null;
+            } else {
+                T elmt = arr[front];
+                arr[front] = null;
+                nextFirst = front;
+                front = IncreaseIndex(nextFirst);
+                size -= 1;
+                return elmt;
+            }
+        }
+    }
+
+    public T removeLast() {
+        if (arr.length >= 16) {
+            if ((float)(size - 1) / arr.length < 0.25) {
+                resize(arr.length / 2);
+            }
+            nextLast = DecreaseIndex(nextLast);
+            T elmt = arr[nextLast];
+            arr[nextLast] = null;
+            size -= 1;
+            return elmt;
+        } else {
+            if (size == 0) {
+                return null;
+            } else {
+                nextLast = DecreaseIndex(nextLast);
+                T elmt = arr[nextLast];
+                arr[nextLast] = null;
+                size -= 1;
+                return elmt;
+            }
+        }
+    }
+
+    public boolean equals (Object o) {
+        if (!(o instanceof  ArrayDeque<?>)) {
+            return false;
+        }
+        if ((( ArrayDeque<?>) o).size() != size()) {
+            return false;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (get(i) != (( ArrayDeque<?>) o).get(i))  {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int DecreaseIndex(int i) {
+        if (i == 0) {
+            return arr.length - 1;
+        }
+        return i - 1;
+    }
+
+    private int IncreaseIndex(int i ) {
+        if (i == arr.length - 1) {
+            return 0;
+        }
+        return i + 1;
     }
 
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -7,5 +7,105 @@ import java.util.Optional;
 import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
+    @Test
+    public void addFirstTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        for (int i = 0; i < 20; ++i) {
+            a.addFirst(i);
+        }
+        a.printDeque();
+        int s = a.size();
+        /*
+        for (int i = 0; i < s; ++i) {
+            int m = a.removeFirst();
+            a.printDeque();
+        }
+         */
+        for (int i = 0; i < s; ++i) {
+            int m = a.removeLast();
+            a.printDeque();
+        }
+    }
+
+    @Test
+    public void addLastTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        for (int i = 0; i < 17; ++i) {
+            a.addLast(i);
+        }
+        int s = a.size();
+        for (int i = 0; i < s; ++i) {
+            int m = a.removeFirst();
+            a.printDeque();
+        }
+    }
+
+    @Test
+    public void addTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        for (int i = 0; i < 20; ++i) {
+            a.addLast(i);
+            a.addFirst(i + 1);
+        }
+        int s = a.size();
+        for (int i = 0; i < s; ++i) {
+            //int m = a.removeFirst();
+            int m = a.removeLast();
+            a.printDeque();
+        }
+        a.printDeque();
+    }
+
+    @Test
+    public void getAndprintDequeTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        /* 3 4 5 6 7 0 1 2 */
+        for (int i = 0; i < 8; ++i) {
+            a.addLast(i);
+            int m = a.get(i);
+            assertEquals(m, i);
+        }
+        a.printDeque();
+        int s = a.size();
+        for (int i = 0; i < s; ++i) {
+            int m = a.removeFirst();
+            a.printDeque();
+            assertEquals(m, i);
+        }
+    }
+
+    @Test
+    public void printDequeTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        a.addFirst(9);
+        a.addLast(8);
+        a.addFirst(1);
+        a.addLast(7);
+        a.printDeque();
+        /* 1 9 8 7 */
+        int m;
+        m = a.removeFirst();
+        a.printDeque();
+        assertEquals(m, 1);
+        m = a.removeFirst();
+        a.printDeque();
+        assertEquals(m, 9);
+    }
+
+    @Test
+    public void equalsTest() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        ArrayDeque<Integer> b = new ArrayDeque<>();
+        a.addLast(1);
+        b.addFirst(2);
+        boolean bo = a.equals(b);
+        assertEquals(bo, false);
+        a.addFirst(2);
+        b.addLast(1);
+        bo = a.equals(b);
+        assertEquals(bo, true);
+    }
+
+
 
 }
